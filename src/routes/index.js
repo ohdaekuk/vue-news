ac654ff6157d8af3e855d90c21d42834ec274b77
ohdaekuk <https://github.com/ohdaekuk>
2023-03-09 19:54:23 +0900

    캡틴판교

---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+
+Vue.use(VueRouter);
+
+import Ask from "../views/AskView.vue";
+import Jobs from "../views/JobsView.vue";
+import News from "../views/NewsView.vue";
+import User from "../views/UserView.vue";
+import Item from "../views/ItemView.vue";
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      // path : url 주소
+      path: "/news",
+
+      // component : url 주소로 요청이 왔을 때 표시 될 컴포넌트
+      component: News,
+    },
+    {
+      path: "/jobs",
+      component: Jobs,
+    },
+    {
+      path: "/ask",
+      component: Ask,
+    },
+    {
+      path: "/user",
+      component: User,
+    },
+    {
+      path: "/item",
+      component: Item,
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/NewsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(NewsData, i) in news" :key="i">
+      {{ i + 1 }} &nbsp; {{ NewsData.title }} <br /><br />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      news: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => (this.news = response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style></style>
